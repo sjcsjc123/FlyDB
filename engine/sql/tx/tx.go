d00a796c2ab66e30b87bb8a913e70d06fec4c5fe
sjcsjc123 <1401189096@qq.com>
2023-10-30 09:53:08 +0800

    init sql parse

--- engine/sql/tx/tx.go
+++ engine/sql/tx/tx.go
@@ -1,5 +1,11 @@
 package tx
 
+import "fmt"
+
 //Transaction Manager: Handle the opening, committing and rolling back of transactions.
 //Concurrency Control: Ensure the correctness of concurrent transactions, which may use lock or timestamp mechanisms.
 //Log Manager: Maintain a persistent log for recovery operations and to ensure data integrity.
+
+func Test() {
+	fmt.Println("Test.....")
+}
